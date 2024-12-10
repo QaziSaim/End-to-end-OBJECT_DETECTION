4d33b52f6197ea41069d992fc2e76c7579fabc1a
Saim <qazisaim121@gmail.com>
2024-12-10 18:11:18 +0530

    data collection write

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'objectdetection',
+    version= '0.0.1',
+    author= 'Sahim',
+    author_email= 'kazisahim121@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
