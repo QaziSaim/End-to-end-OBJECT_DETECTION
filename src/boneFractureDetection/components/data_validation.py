f646df42c879a30e04400d02d1667eb9218cbf19
Saim <qazisaim121@gmail.com>
2026-04-03 20:23:14 +0530

    validation complete

--- src/boneFractureDetection/components/data_validation.py
+++ src/boneFractureDetection/components/data_validation.py
@@ -51,4 +51,57 @@ class DataValidation:
         except Exception as e:
             raise CustomException(e, sys)
     
+    def check_missing_labels(self):
+        try:
+            logger.info('Checking missing labels...')
+
+            for split in ['train','test','valid']:
+                img_path = os.path.join(self.data_path,split,'images')
+                lbl_path = os.path.join(self.data_path,split,'labels')
+
+                images = os.listdir(img_path)
+                labels = os.listdir(lbl_path)
+
+                label_set = set(labels)
+                for img in images:
+                    label_name = (
+                        img.replace('.jpg','.txt').replace('.png','.txt').replace('.jpeg','.txt')
+
+                    )
+
+                    if label_name not in label_set:
+                        logger.warning(f'Missing label for image {img}')
+            logger.info('Missing label check completed')
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def check_empty_files(self):
+        try:
+            logger.info('Checking for empty labels files...')
+
+            for split in ['train','test','valid']:
+                lbl_path = os.path.join(self.data_path, split, 'labels')
+
+                for file in os.listdir(lbl_path):
+                    file_path = os.path.join(lbl_path, file)
+
+                    if os.path.getsize(file_path) == 0:
+                        logger.warning(f'Empty label files: {file_path}')   
+        except Exception as e:
+            raise CustomException(e, sys)
+    
+    def run_validation(self):
+        try:
+            logger.info('Starting Data Validation Pipeline...')
+
+            self.validate_structure()
+            self.validate_images_labels_counts()
+            self.check_missing_labels()
+            self.check_empty_files()
+
+            logger.info('Data Validation Completed Successfully')
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+
     
